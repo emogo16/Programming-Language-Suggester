2ea2f85c4ec6dd0b45f9b5c2d8355e3384e42c8e
Erika Mogollon <fany200025@hotmail.com>
2022-01-29 21:01:52 -0800

    WIP

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function revise(){
+  var questionNum1 = document.quizp.questionNum1.value;
+  var questionNum2 = document.quizp.questionNum2.value;
+  var questionNum3 = document.quizp.questionNum3.value;
+  var questionNum4 = document.quizp.questionNum4.value;
+  var questionNum5 = document.quizp.questionNum5.value;
+
+}
+
+document.getElementById("afterSubmit!").style.visibility = "visible";
+document.getElementById("typeOfLanguage").innerHTML = "The results indicate you are"
