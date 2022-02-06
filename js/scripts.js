e1b675ddd68855539237e6b455c42a0ba6f18f52
Erika Mogollon <fany200025@hotmail.com>
2022-02-05 23:16:28 -0800

    it will add fnishing touches and work properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,40 @@
 $(document).ready(function() {
-  $("form#pquiz").submit(function() {
-    const questionNum1 = $(document.quizp.questionNum1).val();
-    const questionNum2 = $(document.quizp.questionNum2).val();
-    const questionNum3 = $(document.quizp.questionNum3).val();
-    const questionNum4 = $(document.quizp.questionNum4).val();
-    const questionNum5 = $(document.quizp.questionNum5).val();
-    // const yourLanguage = 0
+  $("form#pquiz").submit(function(event) {
+    event.preventDefault();
+    const questionNum1 = $('input[name="questionNum1"]:checked').val();
+    const questionNum2 = $('input[name="questionNum2"]:checked').val();
+    const questionNum3 = $('input[name="questionNum3"]:checked').val();
+    const questionNum4 = $('input[name="questionNum4"]:checked').val();
+    const questionNum5 = $('input[name="questionNum5"]:checked').val();
+    let yourLanguage = 0;
 
-    
     if (questionNum1 === "Elephant") {
       yourLanguage++;
-}
-    if(questionNum2 === "Sushi") {
+    }
+    
+    if (questionNum2 === "Sushi") {
       yourLanguage++;
-}
-    if(questionNum3 === "Football") {
+    }
+    if (questionNum3 === "Football") {
       yourLanguage++;
-}
-    if(questionNum4 === "Gaming"){
+    }
+    if (questionNum4 === "Gaming") {
       yourLanguage++;
-}
-    if(questionNum5 === "Horror"){
+    }
+    if (questionNum5 === "Horror") {
       yourLanguage++;
     }
     
-    const result;
-
-    if (yourLanguage === 0) {
-      result = Python;
+    if (yourLanguage > 0 && yourLanguage <= 3) {
+      alert("Based on your personality you will love Ruby");
+      $("#Ruby").show();
+    } else if (yourLanguage === 0) {  
+      alert("After summing up your results based on your personality you will love Python");
+      $("#Python").show();
+    } else {
+      alert("Your personality indicates you will love Java");
+      $("#Java").show();
     }
-
-    if (yourLanguage > 0 && yourLanguage < 5) {
-      result = Java;
-    }
-
-    if (yourLanguage === 5) {
-      result= Ruby
-    }
-    
-    event.preventDefault();
   });
 
 });
-
-// document.getElementById("submit").style.visibility = "visible";
-// document.getElementById("yourLanguage").innerHTML = "The results indicate you are " + yourLanguage + "yourLanguage";
